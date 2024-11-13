38c0d35d44da54ad298b6151838229dc5085b1de
Raffa144 <mc.13.mirella@gmail.com>
2024-11-13 19:58:59 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+   'Programação',
+   'O que é Python?',
+   'O Python é uma linguagem de programação'
+)
+
+ criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+ )
+
+ criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+ )
+
+ criaCartao(
+   'Lingua Estrangeira',
+   'Como se diz estou com fome em Italiano?',
+   'Estou com fome em italiano é Ho fame'
+)
+
+criaCartao(
+   'Lingua Estrangeira',
+   'Como se diz qual seu nome em Francês?',
+   'Qual seu nome em Francês e quel est ton nom'
+)
+
+criaCartao(
+   'Lingua Estrangeira',
+   'Como se diz oi em Inglês?',
+   'Oi em Inglês é Hi (RAI)'
+)
+
+criaCartao(
+   'Geografia',
+   'Qual a capital da França?',
+   'A capital da França é Paris'
+)
+
+criaCartao(
+   'Geografia',
+   'Qual a capital do Japão?',
+   'A capital do Japão é Tóquio'
+)
+
+criaCartao(
+   'Geografia',
+   'Qual a capital da Austrália?',
+   'A capital da Austrália é Canberra '
+)
+
+criaCartao(
+   'Matemática',
+   'Qual a raiz quadrada de 144',
+   'A raiz quadrada de 144 é 12'
+)
+
+criaCartao(
+   'Matemática',
+   'Quanto é 250x460?',
+   '115,000'
+)
+
+criaCartao(
+   'Matemática',
+   'O que é um número primo?',
+   'Um número primo é um número natural que é maior que 1 e possui apenas dois divisores: 1 e ele próprio. Por exemplo, 2, 3, 5, 7, 11 são números primos.'
+)
